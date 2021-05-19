9238d26cb2b649cbadcce4e954549e6b1a1b6c76
Austin Flatt <austindflatt@gmail.com>
2021-05-18 23:53:55 -0400

    deleted gesture py file, as it was unneeded

--- player.py
+++ player.py
@@ -1,7 +1,2 @@
 class Player:
 
-    def __init__(self):
-        self.gestures = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
-        self.selected_gesture = ''
-        self.name = ''
-        self.score = int
