36db19903387d6f1670110781f7941f10ab2fb64
Austin Flatt <austindflatt@gmail.com>
2021-05-19 00:06:11 -0400

    made class for player.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from game import Game
+import game
 
 if __name__ == '__main__':
     pass
--- player.py
+++ player.py
@@ -1,2 +1,8 @@
-class Player:
+from human import Human
 
+class Player(Human):
+    def __init__(self):
+        super().__init__()
+        self.name = input('Enter name')
+        self.move = ''
+        user_choice = ['rock', 'paper', 'scissors', 'lizard', 'spock']
