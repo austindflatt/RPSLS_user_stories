b28d07882961ebaaca7c9408befaa0071f62cd55
Austin Flatt <austindflatt@gmail.com>
2021-05-19 00:10:56 -0400

    finish computer.py

--- computer.py
+++ computer.py
@@ -2,6 +2,8 @@ from human import Human
 from player import Player
 import random
 
-class Computer:
+class Computer(Human):
     def __init__(self):
-        pass
+        super().__init__()
+        self.name = 'computer'
+        self.move = ''
